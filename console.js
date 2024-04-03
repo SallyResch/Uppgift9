f496a1e450553e30f71f45c51731b74f56c9748c
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-04-03 13:49:27 +0200

    added functionality for bishBosh as intended in first bishBosh

--- console.js
+++ console.js
@@ -1,6 +1,20 @@
-var numbers = [];
+function bishBosh() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 4 === 0) {
+            console.log('Bish-Bosh');
+        } else if (i % 3 === 0) {
+            console.log('Bish');
+        } else if (i % 4 === 0) {
+            console.log('Bosh');
+        } else {
+            console.log(i);
+        }
+    }
+}
+bishBosh();
+/* var numbers = [];
 for (var i = 1; i <= 100; i++) {
     numbers.push(i);
 }
 
-console.log(numbers);
+console.log(numbers); */
