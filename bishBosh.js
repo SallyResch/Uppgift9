174cf8b4cd6421ea2d55bce017503168e03c9f73
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-04-04 10:28:53 +0200

    Almost done with bishbosh2.0

--- bishBosh.js
+++ bishBosh.js
@@ -1,9 +1,25 @@
-function generateBishBosh() {
+function validateNumbers() {
     var endNumber = parseInt(document.getElementById("endNumber").value);
     var bishNumber = parseInt(document.getElementById("bish").value);
     var boshNumber = parseInt(document.getElementById("bosh").value);
-    document.getElementById("results").innerHTML = "";
+
+    if (endNumber < 0 || bishNumber < 0 || boshNumber < 0) {
+        alert("Input numbers cannot be negative. Please enter positive numbers.");
+        return false;
+    }
     
+    return true;
+}
+function generateBishBosh() {
+    if (!validateNumbers()) {
+        return;
+    }
+    var endNumber = parseInt(document.getElementById("endNumber").value);
+    var bishNumber = parseInt(document.getElementById("bish").value);
+    var boshNumber = parseInt(document.getElementById("bosh").value);
+
+    document.getElementById("results").innerHTML = "";
+
     for (var i = 1; i <= endNumber; i++) {
         var output = ""; 
         
@@ -24,7 +40,6 @@ function generateBishBosh() {
         document.getElementById("results").innerHTML += output + ", ";
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('input[type="submit"]').addEventListener('click', generateBishBosh);
